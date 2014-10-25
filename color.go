02b57a4af92b952ff9e707902ffad179e65ab061
Tobias Haar <tobias.haar@googlemail.com>
2014-10-25 12:27:46 +0100

    added documentation for color struct

--- color.go
+++ color.go
@@ -1,15 +1,22 @@
 package three
 
+// Color stores color information for the red, green and blue channel.
 type Color [3]float32
 
+// R returns the value for the red channel.
+// Value can be between 0 and 1
 func (c Color) R() float32 {
 	return c[0]
 }
 
+// G returns the value for the green channel.
+// Value can be between 0 and 1
 func (c Color) G() float32 {
 	return c[1]
 }
 
+// B returns the value for the blue channel.
+// Value can be between 0 and 1
 func (c Color) B() float32 {
 	return c[2]
 }
