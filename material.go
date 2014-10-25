4a2bcb30b1ac44678bb9f1d73942169a4811fcfb
Tobias Haar <tobias.haar@googlemail.com>
2014-10-25 13:26:43 +0100

    fixed typo

--- material.go
+++ material.go
@@ -3,7 +3,7 @@ package three
 // Material is the interface which defines the appearance of 3D objects.
 // For example: solid color, textured, etc.
 //
-// Bug(tobscher) This interface is to big and needs breaking down into smaller pieces.
+// Bug(tobscher) This interface is too big and needs breaking down into smaller pieces.
 // For example: Colored, Textured, etc.
 type Material interface {
 	generateColorBuffer(int) []float32
